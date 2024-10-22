66d971073cb7c26ccfc2840aaa8d710d5539028b
Celve <celve03@gmail.com>
2024-10-22 16:50:56 -0700

    feat: add variance into static workload

--- src/workload/static.py
+++ src/workload/static.py
@@ -1,9 +1,13 @@
+import random
+
 from src.workload.utils import Workload
 
 
 class StaticWorkload(Workload):
     def __init__(self, length: int) -> None:
         self.length = length
+        self.variance = length // 8
 
     def get(self, offset: int, length: int) -> list[str]:
-        return ["a" * 8 * self.length] * length
+        result_length = self.length + random.randint(-self.variance, self.variance)
+        return ["a" * 8 * result_length] * length
